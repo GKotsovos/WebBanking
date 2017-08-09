1e050ded78aa89f670fd7ce283a8547b3c76f1ef
Gerasimos Kotsovos <kotsovos.gerasimos@gmail.com>
2017-08-09 23:04:41 +0300

    Fixed appropriate card's service tab to be active

--- src/routes/root/routes/Banking/routes/Cards/routes/CreditCards/routes/DetailedCard/components/CreditCardServicesTabs/CreditCardServicesTabs.jsx
+++ src/routes/root/routes/Banking/routes/Cards/routes/CreditCards/routes/DetailedCard/components/CreditCardServicesTabs/CreditCardServicesTabs.jsx
@@ -4,10 +4,14 @@ import './CreditCardServicesTabs.css';
 export const CreditCardServicesTabs = ({ linkTo }) => (
   <div className="">
     <ul id="creditCardServicesTabs" className="nav nav-tabs text-center">
-      <li className="serviceTab active" onClick={() => linkTo('/banking/cards/creditcards/card')}>
+      <li
+        className={`serviceTab ${window.location.href.endsWith('card') ? 'active' : ''}`}
+        onClick={() => linkTo('/banking/cards/creditcards/card')}>
         <a href="#cardHistory" aria-controls="history" role="tab" data-toggle="tab">Κινήσεις</a>
       </li>
-      <li className="serviceTab" onClick={() => linkTo('/banking/cards/creditcards/card/payment')}>
+      <li
+        className={`serviceTab ${window.location.href.endsWith('payment') ? 'active' : ''}`}
+        onClick={() => linkTo('/banking/cards/creditcards/card/payment')}>
         <a id="cardPaymentTab" href="#cardPayment" aria-controls="payment" role="tab" data-toggle="tab">Πληρωμή</a>
       </li>
     </ul>
