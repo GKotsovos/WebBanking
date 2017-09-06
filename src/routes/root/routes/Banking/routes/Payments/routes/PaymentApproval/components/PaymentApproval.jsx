98961e694617ef45e9ae2f4c4248fb95e5a54240
Gerasimos Kotsovos <kotsovos.gerasimos@gmail.com>
2017-09-06 23:44:37 +0300

    Completed Payment route(forgot to commit)

--- src/routes/root/routes/Banking/routes/Payments/routes/PaymentApproval/components/PaymentApproval.jsx
+++ src/routes/root/routes/Banking/routes/Payments/routes/PaymentApproval/components/PaymentApproval.jsx
@@ -1,61 +1,82 @@
-import React from 'react';
+import React, { Component, PropTypes }  from 'react';
 import TransactionApprovalHeader from 'routes/root/routes/Banking/routes/components/TransactionApprovalHeader'
 import SimpleTransactionApprovalRow from 'routes/root/routes/Banking/routes/components/SimpleTransactionApprovalRow'
 import MoneyTransactionApprovalRow from 'routes/root/routes/Banking/routes/components/MoneyTransactionApprovalRow'
 import TransactionApprovalButtons from 'routes/root/routes/Banking/routes/components/TransactionApprovalButtons'
 import './PaymentApproval.css';
 
-export const PaymentApproval = () => (
-  <form id="paymentApprovalForm">
+class PaymentApproval extends Component {
+  render() {
+    const {
+      transactionForm,
+      payment
+    } = this.props;
 
-    <div id="paymentApprovalTable" className="form-group">
-      <table className="table table-bordered">
-        <thead>
-          <TransactionApprovalHeader
-            title='Στοιχεία Πληρωμής'
-          />
-        </thead>
-        <tbody>
-          <SimpleTransactionApprovalRow
-            title='Λογαριασμός χρέωσης'
-            value={transactionForm.debitAccount.value}
-          />
-          <SimpleTransactionApprovalRow
-            title='Κωδικός πληρωμής'
-            value={transactionForm.paymentCode.value}
-          />
-          <SimpleTransactionApprovalRow
-            title='Όνομα οργανισμού'
-            value={transactionForm.organization.name}
-          />
-          <MoneyTransactionApprovalRow
-            title='Ποσό πληρωμής'
-            amount={transactionForm.amount.value}
-            currency={transactionForm.currency.value}
-          />
-          <MoneyTransactionApprovalRow
-            title='Έξοδα πληρωμής'
-            amount={transactionForm.charges}
-            currency={transactionForm.currency.value}
-          />
-          <MoneyTransactionApprovalRow
-            title='Σύνολο χρέωσης λογαριασμού'
-            amount={parseFloat(transactionForm.amount.value) + parseFloat(transactionForm.charges)}
-            currency={transactionForm.currency.value}
-          />
-          <SimpleTransactionApprovalRow
-            title='Ημερομηνία εκτέλεσης'
-            value={transactionForm.date.asapTransaction ? transactionForm.date.asapText : transactionForm.date.view}
-          />
-        </tbody>
-      </table>
-    </div>
+    let beneficiary = transactionForm.paymentSelections.paymentMethod;
+    if (transactionForm.paymentSelections.paymentMethod.includes('AGILE')) {
+      beneficiary = 'AGILE BANK'
+    }
 
-    <TransactionApprovalButtons
-      completeTransaction={() => payment(transactionForm)}
-    />
+    let paymentCodeTitle = 'Κωδικός πληρωμής';
+    if (transactionForm.paymentSelections.paymentMethod.includes('ΚΑΡΤΑ')) {
+      paymentCodeTitle = 'Αριθμός κάρτας'
+    } else if (transactionForm.paymentSelections.paymentMethod.includes('ΔΑΝΕΙΟ')) {
+      paymentCodeTitle = 'Αριθμός δανείου'
+    }
 
-  </form>
-)
+    return (
+      <form id="paymentApprovalForm">
+
+        <div id="paymentApprovalTable" className="form-group">
+          <table className="table table-bordered">
+            <thead>
+              <TransactionApprovalHeader
+                title='Στοιχεία Πληρωμής'
+              />
+            </thead>
+            <tbody>
+              <SimpleTransactionApprovalRow
+                title='Λογαριασμός χρέωσης'
+                value={transactionForm.debitAccount.value}
+              />
+              <SimpleTransactionApprovalRow
+                title='Δικαιούχος πληρωμής'
+                value={beneficiary}
+              />
+              <SimpleTransactionApprovalRow
+                title={paymentCodeTitle}
+                value={transactionForm.paymentCode.value}
+              />
+              <MoneyTransactionApprovalRow
+                title='Ποσό πληρωμής'
+                amount={transactionForm.amount.value}
+                currency={transactionForm.currency.value}
+              />
+              <MoneyTransactionApprovalRow
+                title='Έξοδα πληρωμής'
+                amount={transactionForm.charges}
+                currency={transactionForm.currency.value}
+              />
+              <MoneyTransactionApprovalRow
+                title='Σύνολο χρέωσης λογαριασμού'
+                amount={parseFloat(transactionForm.amount.value) + parseFloat(transactionForm.charges)}
+                currency={transactionForm.currency.value}
+              />
+              <SimpleTransactionApprovalRow
+                title='Ημερομηνία εκτέλεσης'
+                value={transactionForm.date.asapTransaction ? transactionForm.date.asapText : transactionForm.date.view}
+              />
+            </tbody>
+          </table>
+        </div>
+
+        <TransactionApprovalButtons
+          completeTransaction={() => payment(transactionForm)}
+        />
+
+      </form>
+    )
+  }
+}
 
 export default PaymentApproval
