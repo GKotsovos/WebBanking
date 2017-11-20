9f6fd89f09f298dc5ef98b1ab36d39d4493b79ff
Gerasimos Kotsovos <kotsovos.gerasimos@gmail.com>
2017-11-20 20:00:08 +0200

    Added usefull link's content

--- src/routes/root/routes/Home/components/MainPanel/Panels/UsefullLinks/UsefullLinks.jsx
+++ src/routes/root/routes/Home/components/MainPanel/Panels/UsefullLinks/UsefullLinks.jsx
@@ -1,11 +1,35 @@
 import React, { PropTypes } from 'react'
 import FontAwesome from 'react-fontawesome'
+import Link from './Link';
+import localizationText from './localizationText';
 import './UsefullLinks.css'
 
-export const UsefullLinks = ({ changePanel }) => (
-  <div id="" className="">
+export const UsefullLinks = ({ language, changePanel }) => (
+  <div id="usefullLinks" className="">
     <FontAwesome className="closePanel" name="window-close-o" onClick={() => changePanel('NEWS')} />
-
+    <h3 id="infoTittle">{localizationText[language].usefullLinksTitle}</h3>
+    <ul>
+      <Link
+        title={localizationText[language].bankOfGreece}
+        link="http://www.bankofgreece.gr"
+      />
+      <Link
+        title={localizationText[language].bicMatching}
+        link="http://www.bankofgreece.gr/Pages/el/PaymentsSystems/SEPA/BICcodeGr.aspx"
+      />
+      <Link
+        title={localizationText[language].ibanValidator}
+        link="https://www.iban.com"
+      />
+      <Link
+        title={localizationText[language].bicSearch}
+        link="https://transferwise.com/us/swift-codes"
+      />
+      <Link
+        title={localizationText[language].exchangeRates}
+        link="http://www.xe.com"
+      />
+    </ul>
   </div>
 )
 
