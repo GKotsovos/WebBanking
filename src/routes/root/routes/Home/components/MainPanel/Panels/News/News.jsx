bdfc563967991f0706124cc274fc0feae75f2bfe
Gerasimos Kotsovos <kotsovos.gerasimos@gmail.com>
2017-12-03 23:55:20 +0200

    Fixed bug of carousel not starting after unmount

--- src/routes/root/routes/Home/components/MainPanel/Panels/News/News.jsx
+++ src/routes/root/routes/Home/components/MainPanel/Panels/News/News.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
+import React, { Component }  from 'react'
 import './News.css'
 
-export const News = () => (
-  <div id="myCarousel" className="carousel slide hidden-xs" data-ride="carousel">
+class News extends Component {
+  componentDidMount() {
+    $('#myCarousel').carousel({ interval: 5000, cycle: true });
+  }
 
-    <ol className="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" className="active" data-interval="5000"></li>
-      <li data-target="#myCarousel" data-slide-to="1" data-interval="5000"></li>
-      <li data-target="#myCarousel" data-slide-to="2" data-interval="5000"></li>
-    </ol>
+  render() {
+    return (
+      <div id="myCarousel" className="carousel slide hidden-xs" data-ride="carousel">
 
-    <div className="carousel-inner" role="listbox">
-      <div className="item active">
-        <img src={require('./news/news1.png')}/>
+        <ol className="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" className="active" data-interval="5000"></li>
+          <li data-target="#myCarousel" data-slide-to="1" data-interval="5000"></li>
+          <li data-target="#myCarousel" data-slide-to="2" data-interval="5000"></li>
+        </ol>
+
+        <div className="carousel-inner" role="listbox">
+          <div className="item active">
+            <img src={require('./news/news1.png')}/>
+          </div>
+
+          <div className="item">
+            <img src={require('./news/news2.png')}/>
+          </div>
+
+          <div className="item">
+            <img src={require('./news/news3.png')}/>
+          </div>
+        </div>
+
       </div>
-
-      <div className="item">
-        <img src={require('./news/news2.png')}/>
-      </div>
-
-      <div className="item">
-        <img src={require('./news/news3.png')}/>
-      </div>
-    </div>
-
-  </div>
-)
+    )
+  }
+}
 
 export default News
